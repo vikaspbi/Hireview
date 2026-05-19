51aff77fd6c039856c5aa853c2355d518f7b6c19
Vikas Kumar Paswan <vikaskumarpaswan180211@gmail.com>
2026-05-19 12:09:25 +0530

    Create jobs.js

---
+++ api/jobs.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  const query = req.query.q;
+
+  const response = await fetch(
+    `https://jsearch.p.rapidapi.com/search?query=${query}&page=1&num_pages=1`,
+    {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
+        'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+      }
+    }
+  );
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
